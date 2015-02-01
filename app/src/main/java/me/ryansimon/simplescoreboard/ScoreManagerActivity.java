b77dc09704c7d61d7ff82ad1af5ed353835c6df0
ryan-simon <linktothepast212@gmail.com>
2015-01-31 17:33:16 -0800

    setup FAB

--- app/src/main/java/me/ryansimon/simplescoreboard/ScoreManagerActivity.java
+++ app/src/main/java/me/ryansimon/simplescoreboard/ScoreManagerActivity.java
@@ -2,19 +2,30 @@ package me.ryansimon.simplescoreboard;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.melnykov.fab.FloatingActionButton;
+
 
 public class ScoreManagerActivity extends ActionBarActivity {
 
+    /**
+     * Layout vars
+     */
+    RecyclerView mPlayerList;
+    FloatingActionButton mFab;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_manager);
         
         setupToolbar();
+        setupContentList();
     }
 
 
@@ -47,5 +58,24 @@ public class ScoreManagerActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(getString(R.string.title_activity_score_manager));
+    }
+
+    /**
+     * Sets up the content list
+     */
+    private void setupContentList() {
+        // do this first, so we have items to give our RecyclerView
+        //createMockContent();
+
+        // setup our RecyclerView
+        mPlayerList = (RecyclerView) findViewById(R.id.player_list);
+        //recList.setAdapter(new FeedItemAdapter(mFeedItems));
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mPlayerList.setLayoutManager(llm);
+
+        // setup FAB and bind it to RecyclerView
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.attachToRecyclerView(mPlayerList);
     }
 }
